18a4351195d98b22af2f514b069625defad7998a
Jason Mehring <nrgaway@gmail.com>
2015-12-15 22:34:45 -0500

    Update depends setup script to install some items via pip

--- linting-setup.sh
+++ linting-setup.sh
@@ -4,6 +4,10 @@ git clone https://github.com/saltstack/salt-pylint.git
 cd salt-pylint
 sudo python setup.py install
 
-sudo yum install python-modernize python-pip python-pep8 pytho-pylint python-pep257
+sudo yum -y install python-modernize python-pip
 
+pip install --upgrade pip
+sudo pip install --upgrade pep8
+sudo pip install --upgrade pylint
+sudo pip install --upgrade pep257
 sudo pip install --upgrade yapf
