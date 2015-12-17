32506d21ec86c6c58f4861b3151f03b260fb4f21
Jason Mehring <nrgaway@gmail.com>
2015-12-16 22:32:38 -0500

    template:  Souce Fedora's distribution.sh to install packages using yumInstall
    Fixes issue where $YUM0 was not being passed to template-yum.conf when installing
    in fc23 since the yum gets re-directed which prevented the local repo from
    being able to be used.
    
    yumInstall will actually directly use dnf if it's available.

--- template/04_install_qubes_fc20.sh
+++ template/04_install_qubes_fc20.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 # vim: set ts=4 sw=4 sts=4 et :
 
-echo "--> Installing Qubes Salt Management Flavor"
-yum install -c ${SCRIPTSDIR}/../template-yum.conf ${YUM_OPTS} -y --installroot=$(pwd)/mnt qubes-mgmt-salt-dev-vm || RETCODE=1
+source "${SCRIPTSDIR}/distribution.sh"
+
+echo "--> Installing Qubes Salt Management Development Packages"
+yumInstall salt salt-minion qubes-mgmt-salt-dev-vm
