ec27a3ea0076ea231113eaa429dabc756e555133
Jason Mehring <nrgaway@gmail.com>
2015-12-23 05:02:23 -0500

    template: Re-organize BUILDER_PLUGIN templates

--- template/04_install_qubes_fc20.sh
+++ template/04_install_qubes_fc20.sh
@@ -1,7 +1,1 @@
-#!/bin/bash -e
-# vim: set ts=4 sw=4 sts=4 et :
-
-source "${SCRIPTSDIR}/distribution.sh"
-
-echo "--> Installing Qubes Salt Management Development Packages"
-yumInstall salt salt-minion qubes-mgmt-salt-dev-vm
+04_install_qubes_fedora.sh
